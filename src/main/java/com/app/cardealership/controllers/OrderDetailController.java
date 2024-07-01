b98ad867438906b2ecd0c3783cc81d8d14bac562
Gonzalo Quiroga <gonzaquirogasassu@gmail.com>
2024-07-01 10:59:43 -0300

    9. Comentando codigo en OrderDetailController

--- src/main/java/com/app/cardealership/controllers/OrderDetailController.java
+++ src/main/java/com/app/cardealership/controllers/OrderDetailController.java
@@ -1,16 +1,15 @@
 package com.app.cardealership.controllers;
 
-import com.app.cardealership.dto.OrderDetailDto;
 import com.app.cardealership.services.IOrderDetailService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/order-detail")
@@ -19,12 +18,19 @@ public class OrderDetailController {
 
     private final IOrderDetailService orderDetailService;
 
+    //Metodo que devuelve todas los detalles de ordenes registrados en la base de datos
     @GetMapping()
     @Tag(name = "GET")
     @Operation(summary = "Get all orders details")
-    private ResponseEntity<List<OrderDetailDto>>findAll(){
-        return ResponseEntity.ok(orderDetailService.findAll());
+    private ResponseEntity<?> findAll() {
+        try {
+            return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .body(orderDetailService.findAll());
+        } catch (Exception e) {
+            return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .body("No hay detalles de ordenes registrados");
+        }
     }
-
-
 }
